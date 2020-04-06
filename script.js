5ad81a8958572f2e35d135f72328a28e2ffb5658
Randall71 <yannbilos@gmail.com>
2020-04-06 03:41:35 +0000

    Animation du canvas

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 window.onload = function () {
 
-    var canvas = document.createElement('canvas');
-    canvas.width = 900;
-    canvas.height = 600;
-    canvas.style.border = "3px solid yellow"
-    this.document.body.appendChild(canvas)
+    var canvas
+    var ctx
+    var delay = 100
+    var xCoord = 0;
+    var yCoord = 0
 
-    var ctx = canvas.getContext('2d')
-    ctx.fillStyle = "Yellow"
-    ctx.fillRect(30 , 30 , 100 , 50)
+    init()
+    // console.log('test')
+
+    function init() {
+        
+        canvas = document.createElement('canvas');
+        canvas.width = 900;
+        canvas.height = 600;
+        canvas.style.border = "3px solid yellow";
+        this.document.body.appendChild(canvas);
+        ctx = canvas.getContext('2d');
+        refreshCanvas();
+ 
+    }
+
+    function refreshCanvas() {
+        
+        xCoord += 2 
+        yCoord += 2
+        ctx.fillStyle = "Yellow";
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillRect(xCoord, yCoord, 100, 50)
+        setTimeout(refreshCanvas, delay)
+    }
 
 
 }
