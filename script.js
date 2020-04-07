b15f9f6065d77992cc8db77233e338a5336fc481
Randall71 <yannbilos@gmail.com>
2020-04-07 06:24:21 +0000

    Direction du serpent

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
 window.onload = function () {
 
-    var canvas
+    var canvasWidth = 900
+    var canvasHeight = 600
+    var blockSize = 30
     var ctx
     var delay = 100
-    var xCoord = 0;
-    var yCoord = 0
+    var snakee; 
 
     init()
-    // console.log('test')
 
     function init() {
         
-        canvas = document.createElement('canvas');
-        canvas.width = 900;
-        canvas.height = 600;
+        var canvas = document.createElement('canvas');
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
         canvas.style.border = "3px solid yellow";
-        this.document.body.appendChild(canvas);
+        document.body.appendChild(canvas);
         ctx = canvas.getContext('2d');
+        snakee = new Snake([[6, 4], [5, 4], [4, 4]]);
         refreshCanvas();
  
     }
 
     function refreshCanvas() {
-        
-        xCoord += 2 
-        yCoord += 2
-        ctx.fillStyle = "yellow";
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillRect(xCoord, yCoord, 100, 50)
+      
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+        snakee.advance()
+        snakee.draw()
         setTimeout(refreshCanvas, delay)
     }
 
+    function drawBlock(ctx, position) {
+        var x = position[0] * blockSize
+        var y = position[1] * blockSize
+        
+        ctx.fillRect(x , y , blockSize , blockSize)
 
-}
+    }
+
+    function Snake(body) {
+        this.body = body
+        this.draw = function () {
+            ctx.save()
+            ctx.fillStyle = "red";
+            for (var i = 0; i < this.body.length; i++){
+                drawBlock(ctx , this.body[i]);
+            }
+            ctx.restore()
+        }
+        this.advance = function () {
+            var nextPosition = this.body[0].slice()
+            nextPosition[0] += 1
+            this.body.unshift(nextPosition)
+            this.body.pop()
+            // console.log(nextPosition)
+        }
+    }
+} 
